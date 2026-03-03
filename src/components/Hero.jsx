7ab90ea0a695909a7806deed07086c1c43dfe07f
Shravan Padale <spadale771@gmail.com>
2026-03-03 13:53:05 +0530

    ads2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -238,18 +238,7 @@ function HeroSection() {
           </div>
         </div>
       </div>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4863171426090040"
-     crossorigin="anonymous"></script>
-
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-4863171426090040"
-     data-ad-slot="3798458677"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+      
     </section>
   );
 }
